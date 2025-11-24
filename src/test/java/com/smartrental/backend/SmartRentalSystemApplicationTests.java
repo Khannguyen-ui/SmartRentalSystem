07eb069d4bd3d34fa7d6462b13628b55f7e8d003
VanKhan02 <nvk2863hotal@gmail.com>
2025-11-24 20:45:10 +0700

    Khoi tao du an

---
+++ src/test/java/com/smartrental/backend/SmartRentalSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartrental.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartRentalSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
