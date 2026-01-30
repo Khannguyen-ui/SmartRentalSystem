6c72d78e6e5db487fccd87e73552a3bb811805d8
VanKhan02 <nvk2863hotal@gmail.com>
2026-01-30 08:18:17 +0700

    Fix port for Railway deployment

--- src/main/java/com/smartrental/backend/config/CorsConfig.java
+++ src/main/java/com/smartrental/backend/config/CorsConfig.java
@@ -10,8 +10,8 @@ public class CorsConfig implements WebMvcConfigurer {
 @Override
 public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-//            .allowedOriginPatterns("*")
-            .allowedOrigins("http://localhost:5173", "http://127.0.0.1:5173")
+        .allowedOriginPatterns("*")
+//            .allowedOrigins("http://localhost:5173", "http://127.0.0.1:5173")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("*")
             .allowCredentials(true);
